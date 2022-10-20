dd7e5fa7dafd9a796cc7f054862dcf180d56b4bc
Alexander-Puv <sansano4sana@gmail.com>
2022-10-20 10:54:38 +0300

    commit

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -4,11 +4,13 @@ import { postAPI } from '../services/PostService'
 import { PostItem } from './PostItem'
 
 export const PostContainer = () => {
-    const [limit, setLimit] = useState(10)
+    const [limit, setLimit] = useState(100)
     const {data: posts, error, isLoading, refetch} = postAPI.useFetchAllUsersQuery(limit, {
         pollingInterval: 100000
     })
     const [createPost, {}] = postAPI.useCreatePostMutation()
+    const [deletePost, {}] = postAPI.useDeletePostMutation()
+    const [updatePost, {}] = postAPI.useUpdatePostMutation()
 
     useEffect(() => {
         /* setTimeout(() => {
@@ -18,7 +20,15 @@ export const PostContainer = () => {
 
     const handleCreate = async () => {
         const title = prompt()
-        await createPost({title, body: title} as IPost)
+        await createPost({title, body: title} as unknown as IPost)
+    }
+
+    const handleRemove = (post: IPost) => {
+        deletePost(post)
+    }
+
+    const handleUpdate = (post: IPost) => {
+        updatePost(post)
     }
     
     return (
@@ -28,7 +38,7 @@ export const PostContainer = () => {
                 {isLoading && <h1>Loading...</h1>}
                 {error && <h1>Error occured while loading posts</h1>}
                 {posts && posts.map(post => 
-                    <PostItem post={post} key={post.id} />
+                    <PostItem post={post} remove={handleRemove} update={handleUpdate} key={post.id} />
                 )}
             </div>
         </div>
--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -2,14 +2,28 @@ import React, { FC } from 'react'
 import { IPost } from '../models/IPost'
 
 interface PostItemProps {
-    post: IPost
+    post: IPost,
+    remove: (post: IPost) => void,
+    update: (post: IPost) => void
 }
 
-export const PostItem: FC<PostItemProps> = ({post}) => {
+export const PostItem: FC<PostItemProps> = ({post, remove, update}) => {
+    const handleRemove = (event: React.MouseEvent) => {
+        event.stopPropagation()
+        remove(post)
+    }
+
+    const handleUpdate = (event: React.MouseEvent) => {
+        const title = prompt() || ""
+        update({...post, title})
+        event.stopPropagation()
+    }
+
     return (
         <div>
             {post.id}. {post.title}
-            <button>Delete</button>
+            <button onClick={handleRemove}>Delete</button>
+            <button onClick={handleUpdate}>Update</button>
         </div>
     )
 }
